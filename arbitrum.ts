d32dc2d6d19a91748ec2904bacd437d02ebaf819
harsh-98 <harshjniitr@gmail.com>
2024-02-26 16:37:18 +0400

    feat: sdkgo

---
+++ arbitrum.ts
@@ -0,0 +1,99 @@
+
+
+
+import {contractsByNetwork, contractParams, CurveParams} from './src/contracts/contracts'
+import {tokenDataByNetwork} from './src/tokens/token'
+import {priceFeedsByToken} from './src/oracles/priceFeeds'
+import {curveTokens} from './src/tokens/curveLP'
+import { yearnTokens } from './src/tokens/yearn';
+import { convexTokens } from './src/tokens/convex';
+import { balancerLpTokens } from './src/tokens/balancer';
+import { PriceFeedType , PriceFeedData} from './src/oracles/pricefeedType';
+import {RR, removeNotDeployed} from './mainnet';
+import {convexLpTokens} from './src/tokens/convex';
+
+export  function arbitrum(obj : RR ) {
+  obj['network'] = 'arbitrum';
+  //
+  var exchanges : Record<string, string>= contractsByNetwork.Arbitrum;
+  removeNotDeployed(exchanges);
+
+  // //
+  // exchanges['steCRV_POOL'] = '0xDC24316b9AE028F1497c275EB9192a3Ea0f67022';
+  // exchanges['UNISWAPV2_FACTORY'] = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+  // exchanges['UNISWAPV3_QUOTER'] = '0xb27308f9f90d607463bb33ea1bebb41c27ce5ab6';
+  // exchanges['UNISWAPV3_FACTORY'] = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
+  // exchanges['GEARBOX_WETH_POOL'] = '0xB03670c20F87f2169A7c4eBE35746007e9575901';
+  // exchanges['WETH_GATEWAY'] = '0x4F952c4C5415B2609899AbDC2F8F352F600d14D6';
+  // exchanges['GEARBOX_WSTETH_POOL'] = '0xB8cf3Ed326bB0E51454361Fb37E9E8df6DC5C286';
+  // exchanges['WSTETH_GATEWAY'] = '0x5a97e3E43dCBFe620ccF7865739075f92E93F5E4';
+  obj['exchanges'] =exchanges;
+  //
+  //
+  var tokens: Record<string, string> = tokenDataByNetwork.Arbitrum;
+  removeNotDeployed(tokens);
+
+  // //
+  tokens['ETH']= '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+  obj['tokens'] = tokens;
+  //
+  {
+    obj['farmingPools'] = {} as Record<string, string>;
+    // farming tokens
+     ['sdUSDCV3', 'sdWETHV3', 'sdWBTCV3', 'sdDAIV3'].forEach((t) => {
+      if (tokens[t]) {
+        (obj['farmingPools'] as Record<string, string>)[t] = tokens[t];
+    }
+    })
+  }
+
+
+  obj['ids'] = {} ;
+  Object.entries(convexLpTokens).forEach(([key, value]) => {
+  (obj['ids'] as Record<string, number>)[value.symbol] = value.pid;
+  })
+
+    {
+      let curveSymbols = Object.keys(curveTokens);
+      let curvePools = Object.entries(curveTokens).reduce((arr, [curvetoken, data]) =>
+        Object.assign(arr, { [curvetoken]: (contractParams[data.pool] as CurveParams).tokens.length })
+      , {});
+  // for liquidator solidity pathfinder
+  obj['groups'] = {
+    'curvePools' : curvePools,
+    'yearnCurveTokens': Object.entries(yearnTokens)
+    .filter(([, data]) => curveSymbols.includes(data.underlying))
+    .reduce((obj, [token, data]) => 
+      Object.assign(obj, { [token]: data.underlying }),
+      {}),
+    'convexCurveTokens': Object.entries(convexTokens)
+    .filter(([, data]) => curveSymbols.includes(data.underlying))
+    .reduce((obj, [token, data]) => 
+      Object.assign(obj, { [token]: data.underlying }),
+      {}),
+    'balancerTokens': Object.entries(balancerLpTokens).reduce((obj, [baltoken, data]) =>
+      Object.assign(obj, { [baltoken]: data.underlying.length })
+    ,{}),
+  }
+    }
+  //
+
+  { // redstone 
+    type sdkGoRedStone = Record<string, PriceFeedData>;
+    var mains ={} as sdkGoRedStone;
+    var reserves ={} as sdkGoRedStone;
+    for (const [token, details] of Object.entries(priceFeedsByToken)) {
+     let networkRS= details.Arbitrum;
+    if (networkRS?.Main.type == PriceFeedType.REDSTONE_ORACLE) {
+      mains[token] = networkRS?.Main;
+      }
+      if (networkRS?.Reserve?.type == PriceFeedType.REDSTONE_ORACLE) {
+        reserves[token] =networkRS?.Reserve;
+      }
+    }
+    obj['redstone'] = {
+      mains: mains,
+      reserves: reserves,
+    }
+  }
+}
--- sdkgo_tokenmap.ts
+++ sdkgo_tokenmap.ts
@@ -1,82 +1,35 @@
 
 
-import {contractsByNetwork, contractParams, CurveParams} from './src/contracts/contracts'
-import {tokenDataByNetwork} from './src/tokens/token'
-import {curveTokens} from './src/tokens/curveLP'
-import { yearnTokens } from './src/tokens/yearn';
-import { convexTokens } from './src/tokens/convex';
-import { balancerLpTokens } from './src/tokens/balancer';
+import {RR , mainnet} from './mainnet';
+import {arbitrum} from './arbitrum';
 
 let networkName = process.argv[2].toLocaleLowerCase();
 
-type RR = Record<
-string,
-string | Record<string, string | number | Object>
->
-var obj: RR = {};
-switch (networkName) {
-  case "mainnet":
-    obj['network'] = 'mainnet';
-    //
-    var exchanges : Record<string, string>= contractsByNetwork.Mainnet;
-    removeNotDeployed(exchanges);
+function run () {
+  var obj: RR = {};
+  switch (networkName) {
+    case "mainnet":
 
-    // //
-    exchanges['steCRV_POOL'] = '0xDC24316b9AE028F1497c275EB9192a3Ea0f67022';
-    exchanges['UNISWAPV2_FACTORY'] = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-    exchanges['UNISWAPV3_QUOTER'] = '0xb27308f9f90d607463bb33ea1bebb41c27ce5ab6';
-    exchanges['UNISWAPV3_FACTORY'] = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
-    exchanges['GEARBOX_WETH_POOL'] = '0xB03670c20F87f2169A7c4eBE35746007e9575901';
-    exchanges['WETH_GATEWAY'] = '0x4F952c4C5415B2609899AbDC2F8F352F600d14D6';
-    exchanges['GEARBOX_WSTETH_POOL'] = '0xB8cf3Ed326bB0E51454361Fb37E9E8df6DC5C286';
-    exchanges['WSTETH_GATEWAY'] = '0x5a97e3E43dCBFe620ccF7865739075f92E93F5E4';
-    obj['exchanges'] =exchanges;
-    //
-    //
-    var tokens: Record<string, string> = tokenDataByNetwork.Mainnet;
-    removeNotDeployed(tokens);
-
-    // //
-    tokens['ETH']= '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
-    tokens['yvSTETH'] ='0x15a2B3CfaFd696e1C783FE99eed168b78a3A371e';
-    obj['tokens'] = tokens;
-
-    obj['ids'] = {"cvxFRAX3CRV":32,"cvxsteCRV":25,"cvxcrvFRAX":100,"cvxgusd3CRV":10,"cvxcrvPlain3andSUSD":4,"cvx3Crv":9,"cvxLUSD3CRV":33,"cvxOHMFRAXBP":138,"cvxMIM_3LP3CRV":40,"cvxcrvCRVETH":61,"cvxcrvCVXETH":64,"cvxcrvUSDTWBTCWETH":188,"cvxLDOETH":149}
-
-    let curveSymbols = Object.keys(curveTokens);
-    let curvePools = Object.entries(curveTokens).reduce((arr, [curvetoken, data]) =>
-      Object.assign(arr, { [curvetoken]: (contractParams[data.pool] as CurveParams).tokens.length })
-    , {});
-    obj['groups'] = {
-      'curvePools' : curvePools,
-      'yearnCurveTokens': Object.entries(yearnTokens)
-      .filter(([, data]) => curveSymbols.includes(data.underlying))
-      .reduce((obj, [token, data]) => 
-        Object.assign(obj, { [token]: data.underlying }),
-        {}),
-      'convexCurveTokens': Object.entries(convexTokens)
-      .filter(([, data]) => curveSymbols.includes(data.underlying))
-      .reduce((obj, [token, data]) => 
-        Object.assign(obj, { [token]: data.underlying }),
-        {}),
-      'balancerTokens': Object.entries(balancerLpTokens).reduce((obj, [baltoken, data]) =>
-        Object.assign(obj, { [baltoken]: data.underlying.length })
-      ,{}),
-    }
-    break;
-    
-    default:
-      console.log("wrong network");
-      process.exit(1);
-
-}
-if (networkName == "mainnet") {
-  obj["tokens"]["G-BLOCK"] = "0xf568F6C71aE0439B8d3FFD60Ceba9B1DcB5819bF";
-  obj["tokens"]["G-OBS"] = "0x26F7D7509490B292AFBB0443A7A371d345B5f684";
+      mainnet(obj);
+      if (networkName == "mainnet") {
+        var data = (obj["tokens"] as Record<string, string >);
+        data["G-BLOCK"] = "0xf568F6C71aE0439B8d3FFD60Ceba9B1DcB5819bF";
+        data["G-OBS"] = "0x26F7D7509490B292AFBB0443A7A371d345B5f684";
+      }
+      break;
+    case "arbitrum":
+      arbitrum(obj);
+      break;
+      default:
+        console.log("wrong network");
+        process.exit(1);
+  }
+  console.log(JSON.stringify(change(obj)));
 }
 
-console.log(JSON.stringify(change(obj)));
 
+
+run();
 function change(obj: any)  {
 if (Array.isArray(obj)) {
   let ans = obj as Array<any>;
@@ -101,18 +54,7 @@ if (Array.isArray(obj)) {
     return obj
   }
 }
-// function change(tokens: RR, oldName: string, newName: string){
-//   tokens[newName] = tokens[oldName];
-//   delete tokens[oldName];
-// }
 
-function removeNotDeployed(obj: Record<string, string>) {
-    for (const key in obj) {
-      if (obj[key] == 'NOT DEPLOYED') {
-        delete obj[key];
-      }
-    }
-}
 function check(a : string) {
  let  obj = {
     'CURVE_LUSD_POOL':'LUSD3CRV_POOL',
