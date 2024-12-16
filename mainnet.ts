f42c0b0c2ce2a5b1ab3c4a8af4539d83c127d3de
harsh-98 <harshjniitr@gmail.com>
2024-12-16 18:27:33 +0700

    feat: deploy dola contract for partial liq

--- mainnet.ts
+++ mainnet.ts
@@ -51,6 +51,32 @@ export  function mainnet(obj : RR ) {
   tokens['GMX'] ='0x00eee00eee00eee00eee00eee00eee00eee00eee'; // gmx token
   tokens['OP'] ='0x00fff00fff00fff00fff00fff00fff00fff00fff'; // gmx token
   tokens['weETH/ETH'] = '0x8C23b9E4CB9884e807294c4b4C33820333cC613c';
+  let tmpTokens = {
+    'COMP': '0xc00e94Cb662C3520282E6f5717214004A7f26888',
+'DPI': '0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b',
+'FEI': '0x956F47F50A910163D8BF957Cf5846D573E7f87CA',
+'OHM': '0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5',
+'MIM': '0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3',
+'SPELL': '0x090185f2135308BaD17527004364eBcC2D37e5F6',
+'OHMFRAXBP': '0x5271045F7B73c17825A7A7aee6917eE46b0B7520',
+'MIM_3LP3CRV': '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
+'cvxOHMFRAXBP': '0xd8F1B275c320819c7D752ef79988d0780bf00446',
+'cvxMIM_3LP3CRV': '0xabB54222c2b77158CC975a2b715a3d703c256F05',
+'stkcvxOHMFRAXBP': '0x888407AabAfa936B90acF65C4Db19370A01d9bd8',
+'stkcvxMIM_3LP3CRV': '0x1aAbe1B22a290cCB39FD77440D5eb96Cf40079f4',
+'50OHM_50DAI': '0x76FCf0e8C7Ff37A47a799FA2cd4c13cDe0D981C9',
+'50OHM_50WETH': '0xD1eC5e215E8148D76F4460e4097FD3d5ae0A3558',
+'OHM_wstETH': '0xd4f79CA0Ac83192693bce4699d0c10C66Aa6Cf0F',
+'CURVE_MIM_POOL': '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
+'CURVE_OHMFRAXBP_POOL': '0xFc1e8bf3E81383Ef07Be24c3FD146745719DE48D',
+'CONVEX_OHMFRAXBP_POOL': '0x27A8c58e3DE84280826d615D80ddb33930383fE9',
+'CONVEX_MIM3CRV_POOL': '0xFd5AbF66b003881b88567EB9Ed9c651F14Dc4771',
+  };
+  for (const [key, value] of Object.entries(tmpTokens)) {
+    console.log("here");
+    tokens[key] = value;
+  }
+
   obj['tokens'] = tokens;
   //
   {
@@ -66,6 +92,14 @@ export  function mainnet(obj : RR ) {
     obj['ids'] = {} ;
     Object.entries(convexLpTokens).forEach(([key, value]) => {
     (obj['ids'] as Record<string, number>)[value.symbol] = value.pid;
+    })
+    Object.entries({
+      'cvxOHMFRAXBP': 138,
+'cvxMIM_3LP3CRV': 40,
+'MIM_3LP3CRV': 2,
+'OHMFRAXBP': 2,
+    }).forEach(([key, value]) => {
+    (obj['ids'] as Record<string, number>)[key] =value;
     })
 
     {
@@ -91,7 +125,23 @@ export  function mainnet(obj : RR ) {
     ,{}),
   }
     }
-  //
+    Object.entries({
+      'cvxOHMFRAXBP': 'OHMFRAXBP',
+'cvxMIM_3LP3CRV': 'MIM_3LP3CRV',
+'stkcvxOHMFRAXBP': 'OHMFRAXBP',
+'stkcvxMIM_3LP3CRV': 'MIM_3LP3CRV',
+    }).forEach(([key, value]) => {
+     ( (obj['groups']as Record<string, string | number | Object>)['convexCurveTokens'] as Record<string, string>)[key] = value;
+    });
+
+    Object.entries({
+      'OHM_wstETH': 2,
+'50OHM_50DAI': 2,
+'50OHM_50WETH': 2,
+    }).forEach(([key, value]) => {
+      ((obj['groups']as Record<string, string | number | Object>)['balancerTokens'] as Record<string, number>)[key] = value;
+    });
+    //
 
   { // redstone 
     var mains ={
